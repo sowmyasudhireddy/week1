2baaf8477e13c1f42c48e32aeeb5054e4bc772e0
sowmyasudhireddy <42416286+sowmyasudhireddy@users.noreply.github.com>
2018-11-06 22:06:50 +0530

    Add files via upload

---
+++ primenumbers.js
@@ -0,0 +1,19 @@
+function isPrime(num) {
+    for ( var i = 2; i < num; i++ ) {
+        if ( num % i === 0 ) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function display(n) {
+    var arr = [];
+    for ( var i = 3; i < n; i+=2 ) {
+        if ( isPrime(i) ) {
+            arr.push(i);
+        }
+    }
+    console.log("prime numbers upto 100:"+arr); 
+}
+display(100);

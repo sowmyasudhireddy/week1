59929523e62c7c07d781fe4eae8ed45d8aa24d55
sowmyasudhireddy <42416286+sowmyasudhireddy@users.noreply.github.com>
2018-11-06 22:42:29 +0530

    Update addition.js

--- addition.js
+++ addition.js
@@ -1,6 +1,6 @@
 function addition(arr){
     var number1=Number(arr[0]);
-    var number2=Number(arr[1])
+    var number2=Number(arr[1]);
     console.log('sum of '+number1+'and'+number2+'is',(number1+number2));
 }
 addition(process.argv.slice(2));

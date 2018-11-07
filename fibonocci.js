323b20a1d9757b2aa6f3469251ec89aa8b00f7a6
sowmyasudhireddy <42416286+sowmyasudhireddy@users.noreply.github.com>
2018-11-07 08:47:55 +0530

    Update fibonocci.js

--- fibonocci.js
+++ fibonocci.js
@@ -6,3 +6,15 @@ for(i=2;i<=10;i++){
     fib[i]=fib[i-2]+fib[i-1];
     console.log(fib[i]);
 }
+
+output:
+C:\Users\Sowmya Sudhireddy\3D Objects\javascript assignment> node fibonocci.js
+1
+2
+3
+5
+8
+13
+21
+34
+55

2615a90944583d8f2a81cb042423f5c1dc092ca2
sowmyasudhireddy <42416286+sowmyasudhireddy@users.noreply.github.com>
2018-11-07 08:42:11 +0530

    Update addandsub.js

--- addandsub.js
+++ addandsub.js
@@ -7,3 +7,7 @@ var number2=Number(process.argv[3]);
 var sub=number1-number2;
 console.log('substraction is:'+sub);
 
+output:
+PS C:\Users\Sowmya Sudhireddy\3D Objects\javascript assignment> node add.js 5 6
+addition is:11
+substraction is:-1

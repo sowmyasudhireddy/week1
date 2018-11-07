4087669c65297cca204bbb57b8daf46531884213
sowmyasudhireddy <42416286+sowmyasudhireddy@users.noreply.github.com>
2018-11-07 08:45:02 +0530

    Update sorting.js

--- sorting.js
+++ sorting.js
@@ -4,3 +4,7 @@ function sort(arr)
     arr.sort();
     console.log(arr);
 }
+
+output:
+PS C:\Users\Sowmya Sudhireddy\3D Objects\javascript assignment> node sorting.js 6 4 2 9 1
+[ '1', '2', '4', '6', '9' ]
